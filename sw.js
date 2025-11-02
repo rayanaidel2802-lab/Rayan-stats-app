84e02ffbf4ea708b671fc22f0c4973c3e21f4610
rayanaidel2802-lab <rayanaidel2802@gmail.com>
2025-11-02 09:24:31 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'nayar-stats-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retourne le cache si disponible, sinon fetch
+        return response || fetch(event.request);
+      })
+  );
+});
